1c14addabae1f24395f4ca2b44566b4b68e26101
rgrgzz <ict_701@naver.com>
2025-08-01 12:55:38 +0900

    .

--- Main.java
+++ Main.java
@@ -1,62 +1,22 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-
-    static int N, M;
-    static int[][] map;
-    static boolean[][] visited;
-    static int[][] dist;
-
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] input = br.readLine().split(" ");
-        N = Integer.parseInt(input[0]);
-        M = Integer.parseInt(input[1]);
 
-        map = new int[N][M];
-        visited = new boolean[N][M];
-        dist = new int[N][M];
+        int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < M; j++) {
-                map[i][j] = line.charAt(j) - '0';   // 문자 1 → 정수 1
+        int num = 666;
+        int cnt = 0;
+
+        while (true) {
+            if (String.valueOf(num).contains("666")) {
+                cnt++;
             }
+            if (cnt == N) break;
+            num++;
         }
-
-        bfs(0, 0); 
-
-        System.out.println(dist[N - 1][M - 1]); 
-    }
-
-    static void bfs(int x, int y) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {x, y});
-        visited[x][y] = true;
-        dist[x][y] = 1;
-
-        while (!queue.isEmpty()) {
-            int[] pos = queue.poll();
-            int cx = pos[0];
-            int cy = pos[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
-
-                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
-                    if (!visited[nx][ny] && map[nx][ny] == 1) {
-                        queue.offer(new int[] {nx, ny});
-                        visited[nx][ny] = true;
-                        dist[nx][ny] = dist[cx][cy] + 1;
-                    }
-                }
-            }
-        }
+        System.out.println(num);
+        br.close();
     }
 }
--- boj/silver/1436/Main.java
+++ boj/silver/1436/Main.java
@@ -1,36 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        
-        int count = 0;  // 찾은 종말의 수의 개수
-        int num = 666;  // 666부터 시작
-        
+
+        int N = Integer.parseInt(br.readLine());
+
+        int num = 666;
+        int cnt = 0;
+
         while (true) {
-            // 종말의 수인지 확인
-            String str = String.valueOf(num);
-            boolean isApocalypse = false;
-            
-            // 연속된 6이 3개 이상 있는지 확인
-            for (int i = 0; i <= str.length() - 3; i++) {
-                if (str.charAt(i) == '6' && str.charAt(i + 1) == '6' && str.charAt(i + 2) == '6') {
-                    isApocalypse = true;
-                    break;
-                }
+            if (String.valueOf(num).contains("666")) {
+                cnt++;
             }
-            
-            if (isApocalypse) {
-                count++;
-                if (count == n) {
-                    System.out.println(num);
-                    break;
-                }
-            }
+            if (cnt == N) break;
             num++;
         }
+        System.out.println(num);
+        br.close();
     }
 }
